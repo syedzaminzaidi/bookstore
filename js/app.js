25ad59d0852b30421577f39f1fe4d7ea06144e90
syedzaminzaidi <53317688+syedzaminzaidi@users.noreply.github.com>
2020-06-09 19:31:48 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -30,7 +30,6 @@ function removeFromCart(element) {
   const cartBtn = document.querySelectorAll('.store-item-icon');
   cartBtn.forEach(function (btn) {
     btn.addEventListener("click", function (event) {
-      //console.log(event.target);
       if (event.target.parentElement.classList.contains("store-item-icon")) {
         let fullPath = event.target.parentElement.previousElementSibling.src;
         let pos = fullPath.indexOf("img") + 3;
@@ -45,9 +44,6 @@ function removeFromCart(element) {
 
         let finalPrice = price.slice(1).trim(1);
         item.price = finalPrice;
-        //console.log(name);
-        //console.log(finalPrice);
-        //console.log(item);
 
         const cartItem = document.createElement("div");
         cartItem.classList.add("cart-item", "d-flex", "justify-content-between", "text-capitalize", "my-3");
@@ -123,69 +119,7 @@ function check() {
 
 }
 
-// delete items in cart
-/*var deleteItem = document.getElementsByClassName('cart-item-remove')
-//console.log(deleteItem)
-for (var i = 0; i < deleteItem.length; i++) {
-  var dlt = deleteItem[i]
-  dlt.addEventListener('click', function (event) {
-    var btnClicked = event.target
-    btnClicked.parentElement.parentElement.remove()
-    //updateCartTotal()
-  })
-}*/
 
-
-/*function updateCartTotal() {
-  var cartItemContainer = document.getElementsByClassName('cart')
-  var cartRows = cartItemContainer.getElementsByClassName('cart-item')
-  var total1 = 0
-  for (var i = 0; i < cartRows.length; i++) {
-    var cartRow = cartRows[i]
-    var priceElement = cartRow.getElementsByClassName('cart-item-price')
-    var price = parseFloat(priceElement.innerText)
-    total1 = total1 + (price)
-  }
-  document.getElementsByClassName('cart-total').innerText = total1
-}*/
-
-
-
-//search items 
-const sItems = [
-  { id: 0, name: "religion item", img: "img/religion1.jpg", price: 5 },
-  { id: 1, name: "religion item", img: "img/religion2.jpg", price: 5 },
-  { id: 2, name: "religion item", img: "img/religion3.jpg", price: 5 },
-  { id: 2, name: "history item", img: "img/history1.jpg", price: 10 },
-  { id: 2, name: "history item", img: "img/history2.jpg", price: 10 },
-  { id: 2, name: "history item", img: "img/history3.jpg", price: 10 },
-];
-// (function search() {
-//   const searchItems = document.getElementById("search-store-items");
-//   searchItems.addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-
-//     if (itemFound != "") {
-//       const resultSearch = document.getElementById("search-item");
-//       resultSearch.innerHTML = sItems.map(item =>
-//         `<div class="card single-item">
-//             <div class="img-container">
-//               <img src=${item.img} class="card-img-top store-img" alt="">
-//               <span class="store-item-icon">
-//                 <i class="fas fa-shopping-cart"></i>
-//               </span>
-//             </div>
-//             <div class="card-body">
-//               <div class="card-text d-flex justify-content-between text-capitalize">
-//                 <h5 id="store-item-name">${item.name}</h5>
-//                 <h5 class="store-item-value">$<strong id="store-item-pice" class="font-weight-bold">${item.price}</strong></h5>
-//               </div>
-//             </div>
-//           </div>`)
-//     }
-//   })
-// })
 function fun() {
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById("search-item");
@@ -205,4 +139,3 @@ function fun() {
   }
 
 }
-//search item
